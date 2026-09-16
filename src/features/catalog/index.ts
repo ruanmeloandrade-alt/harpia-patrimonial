c7eb90100f848d7d701a059c6262e4e371af8dc8
ruanmeloandrade-alt <ruan.melo.andrade@gmail.com>
2026-09-16 13:34:45 -0300

    [F03] catalogo: exportar adapter de midia

--- src/features/catalog/index.ts
+++ src/features/catalog/index.ts
@@ -1,6 +1,7 @@
 export * from './types';
 export * from './catalogRepository';
 export * from './supabaseCatalogRepository';
+export * from './catalogMediaStorage';
 export * from './publicCatalog';
 export * from './CatalogAdminPage';
 export * from './Front03Workspace';
