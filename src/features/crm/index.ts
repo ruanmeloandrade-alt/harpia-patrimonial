86c1d3399623fdc321b03731a0928e718590028f
ruanmeloandrade-alt <ruan.melo.andrade@gmail.com>
2026-09-16 13:00:19 -0300

    [F04] integration: exportar adapters entre frentes

--- src/features/crm/index.ts
+++ src/features/crm/index.ts
@@ -2,5 +2,7 @@ export * from './domain';
 export * from './repository';
 export * from './service';
 export * from './contracts';
+export * from './front01Adapter';
+export * from './front05Adapter';
 export * from './CrmWorkspace';
 export * from './Front04Workspace';
