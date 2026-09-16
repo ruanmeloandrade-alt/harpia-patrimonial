fba379f7432cad62f2bdd4cbc705a22f031b3820
ruanmeloandrade-alt <ruan.melo.andrade@gmail.com>
2026-09-16 13:55:53 -0300

    [F03] catalogo: exportar factory de runtime

--- src/features/catalog/index.ts
+++ src/features/catalog/index.ts
@@ -2,6 +2,7 @@ export * from './types';
 export * from './catalogRepository';
 export * from './supabaseCatalogRepository';
 export * from './catalogMediaStorage';
+export * from './catalogRuntime';
 export * from './publicCatalog';
 export * from './CatalogAdminPage';
 export * from './Front03Workspace';
