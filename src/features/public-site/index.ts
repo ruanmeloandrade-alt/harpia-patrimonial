7827c51d41a7ba8df2a2e38cd601b5637f191f8a
ruanmeloandrade-alt <ruan.melo.andrade@gmail.com>
2026-09-17 07:38:57 -0300

    [F02] site: remover export duplicado da conta

--- src/features/public-site/index.ts
+++ src/features/public-site/index.ts
@@ -4,10 +4,6 @@ export {
   Front02IntegrationShell,
   type Front02IntegrationShellProps,
 } from './Front02IntegrationShell';
-export {
-  Front02ClientAccountShell,
-  type Front02ClientAccountShellProps,
-} from './Front02ClientAccountShell';
 
 export type {
   PublicAuthBridge,
